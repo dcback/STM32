35ccd89b61a2b5b6f81d017c951e184af1d02be2
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-06 13:40:05 +0900

    Update mySerial_TX_buf_stm.ino

--- Example/SoftwareSerial/TRX_buf/mySerial_TX_buf_stm.ino
+++ Example/SoftwareSerial/TRX_buf/mySerial_TX_buf_stm.ino
@@ -1,5 +1,31 @@
-#include "SoftSerialSTM32.h"
+/*********************************************************
+    mySerial.write(buf, sizeof(buf))     write -> serial
+    mySerial.readBytes(buf, sizeof(buf)) serial -> read
+**********************************************************/
+#include "SoftSerialSTM32.h" 
 
 #define GPIO_SERIAL_RX    PA2
 #define GPIO_SERIAL_TX    PA3
 SoftSerialSTM32 mySerial(GPIO_SERIAL_RX, GPIO_SERIAL_TX);
+
+void setup() {
+    Serial.begin(9600);
+    mySerial.begin(9600);
+}
+
+void loop() {
+    char buf[] = "ABCD:";
+
+    mySerial.write(buf, sizeof(buf));   // sizeof(buf) == 6
+
+    Serial.println(sizeof(buf));
+    Serial.println(buf);
+    Serial.println(buf[0]);
+    Serial.println(buf[1]);
+    Serial.println(buf[2]);
+    Serial.println(buf[3]);
+    Serial.println(buf[4]);
+    Serial.println(buf[5]); // '\n\
+
+    delay(1000);
+}
